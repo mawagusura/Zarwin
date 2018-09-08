9167bd34cf911604ae54bb45985c6abbd470ce81
Edouard Paumier <edouard.paumier@gmail.com>
2018-09-08 09:44:32 +0200

    Contracts - Version 2

---
+++ Zarwin.Shared.Contracts/Output/Result.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System.Linq;
+using System.Text;
+
+namespace Zarwin.Shared.Contracts.Output
+{
+    public class Result
+    {
+        [JsonProperty("waves")]
+        public WaveResult[] Waves { get; }
+        
+        public Result(params WaveResult[] waves)
+        {
+            Waves = waves;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Result;
+
+            if (other == null)
+                return false;
+
+            if (this.Waves.Length != other.Waves.Length)
+                return false;
+
+            return AreAllWavesEqual(this.Waves, other.Waves);
+        }
+
+        private static bool AreAllWavesEqual(WaveResult[] theseWaves, WaveResult[] theirWaves)
+        {
+            return theseWaves.Zip(theirWaves, AreWavesEqual)
+                .All(result => result);
+        }
+
+        private static bool AreWavesEqual(WaveResult thisWave, WaveResult otherWave)
+        {
+            return thisWave.Equals(otherWave);
+        }
+
+        public override int GetHashCode()
+        {
+            // This is not really good in terms of performance, 
+            // but this class should not really be hashed anyway
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            int waveIndex = 1;
+            foreach (var wave in Waves)
+            {
+                builder.AppendLine($"== Wave #{waveIndex++} == ");
+                builder.AppendLine(wave.ToString());
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Zarwin.Shared.Contracts/Output/SoldierState.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+
+namespace Zarwin.Shared.Contracts.Output
+{
+    public class SoldierState
+    {
+        [JsonProperty("id")]
+        public int Id { get; }
+
+        [JsonProperty("level")]
+        public int Level { get; }
+
+        [JsonProperty("healthPoints")]
+        public int HealthPoints { get; }
+
+        public SoldierState(int id, int level, int healthPoints)
+        {
+            Id = id;
+            Level = level;
+            HealthPoints = healthPoints;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as SoldierState;
+
+            if (other == null)
+                return false;
+
+            return Id == other.Id
+                && Level == other.Level
+                && HealthPoints == other.HealthPoints;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Soldier #{Id} : Level={Level}, HP={HealthPoints}";
+        }
+    }
+}

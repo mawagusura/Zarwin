7d2f8e569b9729cf2a8f7cc740fc3a7ea132de3f
Edouard Paumier <edouard.paumier@gmail.com>
2018-09-08 09:51:17 +0200

    Acceptance Tests - Version 1

---
+++ Zarwin.Shared.Tests/IntegratedTests.CompleteScenario.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using Zarwin.Shared.Contracts.Input;
+using Zarwin.Shared.Contracts.Output;
+
+namespace Zarwin.Shared.Tests
+{
+    public partial class IntegratedTests
+    {
+        public static IEnumerable<Scenario> Scenarios
+        {
+            get
+            {
+                yield return new Scenario(
+                    "No Soldier",
+
+                    new Parameters(
+                        1,
+                        new FirstSoldierDamageDispatcher(),
+                        new HordeParameters(1),
+                        new CityParameters(0)),
+
+                    new Result(
+                        new WaveResult(
+                            new TurnResult(
+                                soldiers: new SoldierState[0],
+                                horde: new HordeState(1),
+                                wallHealthPoints: 0))));
+
+
+
+                yield return new Scenario(
+                    "1 Soldier, 1 Zombie, No Wall",
+
+                    new Parameters(
+                        1,
+                        new FirstSoldierDamageDispatcher(),
+                        new HordeParameters(1),
+                        new CityParameters(0),
+                        new SoldierParameters(1, 1)),
+
+                    new Result(
+                        new WaveResult(
+                            new TurnResult(
+                                soldiers: new[] { new SoldierState(1, 1, 4) },
+                                horde: new HordeState(1),
+                                wallHealthPoints: 0),
+                            new TurnResult(
+                                soldiers: new[] { new SoldierState(1, 1, 4) },
+                                horde: new HordeState(1),
+                                wallHealthPoints: 0),
+                            new TurnResult(
+                                soldiers: new[] { new SoldierState(1, 2, 5) },
+                                horde: new HordeState(0),
+                                wallHealthPoints: 0))));
+
+
+
+                yield return new Scenario(
+                    "1 Soldier, 2 Zombies, No Wall",
+
+                    new Parameters(
+                        1,
+                        new FirstSoldierDamageDispatcher(),
+                        new HordeParameters(2),
+                        new CityParameters(0),
+                        new SoldierParameters(1, 1)),
+
+                    new Result(
+                        new WaveResult(
+                            new TurnResult(
+                                soldiers: new[] { new SoldierState(1, 1, 4) },
+                                horde: new HordeState(2),
+                                wallHealthPoints: 0),
+                            new TurnResult(
+                                soldiers: new[] { new SoldierState(1, 1, 4) },
+                                horde: new HordeState(2),
+                                wallHealthPoints: 0),
+                            new TurnResult(
+                                soldiers: new[] { new SoldierState(1, 2, 4) },
+                                horde: new HordeState(1),
+                                wallHealthPoints: 0),
+                            new TurnResult(
+                                soldiers: new[] { new SoldierState(1, 3, 5) },
+                                horde: new HordeState(0),
+                                wallHealthPoints: 0))));
+            }
+        }
+    }
+}
---
+++ Zarwin.Shared.Tests/IntegratedTests.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+using Zarwin.Shared.Contracts;
+using Zarwin.Shared.Contracts.Input;
+using Zarwin.Shared.Contracts.Output;
+
+namespace Zarwin.Shared.Tests
+{
+    public abstract partial class IntegratedTests
+    {
+        public abstract IInstantSimulator CreateSimulator();
+
+        [Theory]
+        [MemberData(nameof(ScenarioData), DisableDiscoveryEnumeration = false)]
+        public void AllScenario(Scenario scenario)
+        {
+            scenario.Run(CreateSimulator());
+        }
+
+        public static object[][] ScenarioData => Scenarios.Select(s => new object[] { s }).ToArray();
+
+        public class Scenario : IXunitSerializable
+        {
+            private string name;
+
+            public override string ToString()
+            {
+                return name;
+            }
+
+            private Parameters input;
+            private Result expectedOuput;
+
+            public Scenario(string name, Parameters input, Result expectedOuput)
+            {
+                this.name = name;
+                this.input = input;
+                this.expectedOuput = expectedOuput;
+            }
+
+            public void Run(IInstantSimulator simulator)
+            {
+                if (input == null || expectedOuput == null)
+                    throw new InvalidOperationException("Cannot execute a scenario without input or expectedOutput");
+
+                var actualOutput = simulator.Run(input);
+
+                Assert.Equal(expectedOuput, actualOutput);
+            }
+
+            #region IXunitSerializable
+
+            [Obsolete("Shouldn't call this directly", true)]
+            public Scenario()
+            {
+            }
+
+            public void Serialize(IXunitSerializationInfo info)
+            {
+                info.AddValue("name", name);
+                info.AddValue("input", JsonConvert.SerializeObject(input));
+                info.AddValue("expectedOutput", JsonConvert.SerializeObject(expectedOuput));
+            }
+
+            public void Deserialize(IXunitSerializationInfo info)
+            {
+                this.name = info.GetValue<string>("name");
+
+                var jsonInput = info.GetValue<string>("input");
+                var jsonExpectedOutput = info.GetValue<string>("expectedOutput");
+                
+                this.input = JsonConvert.DeserializeObject<Parameters>(jsonInput);
+                this.expectedOuput = JsonConvert.DeserializeObject<Result>(jsonExpectedOutput);
+            }
+
+            #endregion IXunitSerializable
+        }
+    }
+}

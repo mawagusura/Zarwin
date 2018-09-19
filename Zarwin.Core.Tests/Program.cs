97bbe06b74f5e1514453b74143ed4a3c88c5448d
mawagusura <amaury-lucas@orange.fr>
2018-09-19 11:06:01 +0200

    Init core & tests projects

---
+++ Zarwin.Core.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zarwin.Core.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

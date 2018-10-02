60e3de3443ca85a8eace90714987452c311e65c3
Lucas Masson <lucmasson97@gmail.com>
2018-10-02 22:01:10 +0200

    Add Wave

--- Zarwin.Core/engine/Wave.cs
+++ Zarwin.Core/engine/Wave.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Zarwin.Core.Entity;
+using Zarwin.Shared.Contracts.Core;
 using Zarwin.Shared.Contracts.Input;
 using Zarwin.Shared.Contracts.Output;
 
@@ -17,39 +18,85 @@ namespace Zarwin.Core.Engine
         private HordeState HordeState() => new HordeState(this.zombies.Count());
 
         private readonly Boolean console;
+        private readonly IDamageDispatcher dispatcher;
 
-
-        public Wave(HordeParameters hordeParameter, City city, Boolean console)
+        public Wave(HordeParameters hordeParameter, City city, IDamageDispatcher dispatcher, Boolean console)
         {
             this.zombies = Enumerable.Repeat(new Zombie(), hordeParameter.Size).ToList();
             this.city = city;
             this.console = console;
+            this.dispatcher = dispatcher;
 
             //Create InitialResult
             this.InitialResult = new TurnResult(city.SoldierState.ToArray(), new HordeState(zombies.Count()), city.WallHealthPoints);
         }
 
-        public void Run()
+        public WaveResult Run()
         {
             //ApproachTurn
             ApproachTurn();
             this.TurnResults.Add(this.CurrentTurnResult());
 
-            //Rounds
-            while (this.zombies.Count() > 0)
+            //SoldierTurns
+            foreach(Soldier soldier in this.city.Soldiers)
             {
-                
-                if (this.console)
+                if (this.zombies.Count() > 0 || soldier.HealthPoints == 0)
                 {
-                    Console.ReadLine();
+                    break;
                 }
-                
+
+                this.SoliderTurn(soldier);
+                this.TurnResults.Add(this.CurrentTurnResult());
             }
-            
+
+            //ZombieTurn
+            this.ZombieTurn();
+            this.TurnResults.Add(this.CurrentTurnResult());
+
+            return new WaveResult(this.InitialResult,this.TurnResults.ToArray());
         }
         
         private TurnResult CurrentTurnResult()
             => new TurnResult(this.city.SoldierState.ToArray(), this.HordeState(), this.city.WallHealthPoints);
+
+        private void SoliderTurn(Soldier soldier)
+        {
+            if(soldier.AttackPoints >= this.zombies.Count)
+            {
+                this.zombies.Clear();
+            }
+            else
+            {
+                for (int i = 0; i < soldier.AttackPoints; i++)
+                {
+                    this.zombies.RemoveAt(0);
+                }
+            }
+
+            if (this.console)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private void ZombieTurn()
+        {
+            if (this.city.WallHealthPoints > 0)
+            {
+                this.city.HurtWall(this.zombies.Count);
+                Console.WriteLine("Zombies attack wall");
+            }
+            else
+            {
+                this.dispatcher.DispatchDamage(this.zombies.Count,this.city.Soldiers);
+                Console.WriteLine("Zombies attack soldiers");
+            }
+
+            if (this.console)
+            {
+                Console.ReadLine();
+            }
+        }
 
         private void ApproachTurn()
         {
